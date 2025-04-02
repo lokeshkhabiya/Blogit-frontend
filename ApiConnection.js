dd9b6b8d4d7a958e59a15bc27a0216ad37772948
lokeshkhabiya <lokeshkhabiya0011@gmail.com>
2025-04-02 14:05:15 +0530

    Complete assignment frontend

--- ApiConnection.js
+++ ApiConnection.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:8080/"
-// export const BASE_URL = "https://"
+// export const BASE_URL = "http://localhost:8080/"
+export const BASE_URL = "https://blogit-backend-k044.onrender.com/"
